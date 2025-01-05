84a781756142b02a51a29e1ab5d04e3596620d4b
Curtis Wirtz <supcurtis@gmail.com>
2025-01-05 00:15:53 -0800

    correctly configure typescript to compile js and serve to local

--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,5 +1,28 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  // Reset the playing of animated gif on every load by appending '?RANDOM_NUMBER' to the src, forcing image load
+  function generateNumber(): string {
+    const digits: string = "0123456789";
+    let result: string = "";
 
-console.log(greet("World"));
+    for (let i = 0; i < 5; i++) {
+      const randomIndex: number = Math.floor(Math.random() * digits.length);
+      result += digits[randomIndex];
+    }
+
+    return result;
+  }
+
+  const resetGif = () => {
+    const gif: HTMLElement | null = document.getElementById("pixel-curtis");
+    console.log(gif);
+    if (gif) {
+      const src: string = gif.src;
+      const timeAndDateNumbers: number = generateNumber();
+      gif.src = "";
+      gif.src = src + `?${timeAndDateNumbers}`;
+    }
+  };
+  resetGif();
+
+  console.log("testing");
+});
