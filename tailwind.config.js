9785e84138cfaf34a2a4490645b0e5481607997b
Curtis Wirtz <supcurtis@gmail.com>
2025-01-04 22:17:10 -0800

    configure tailwind custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,44 @@ module.exports = {
     "./_layouts/**/*.html" // Include layouts directory
   ],
   theme: {
-    extend: {}
+    screens: {
+      "-sm": { max: "599px" },
+      sm: "600px",
+      "-md": { max: "767px" },
+      md: "768px",
+      "-tablet": { max: "899px" },
+      tablet: "900px",
+      "-lg": { max: "1023px" },
+      lg: "1024px",
+      "-xl": { max: "1219px" },
+      xl: "1220px",
+      "-2xl": { max: "1599px" },
+      "2xl": "1600px",
+      "-3xl": { max: "1999px" },
+      "3xl": "2000px"
+    },
+    colors: {},
+    extend: {
+      colors: {
+        inherit: "inherit",
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#fff",
+        black: "#000",
+        base: "#d1e4dd",
+        pank: "#ef7ef7"
+      },
+      maxWidth: {
+        300: "75rem" // 1200px
+      },
+      minWidth: {
+        200: "50rem" // 800px
+      }
+    }
   },
-  plugins: []
+  plugins: [],
+  output: {
+    path: "./assets/css",
+    filename: "styles.css"
+  }
 };
