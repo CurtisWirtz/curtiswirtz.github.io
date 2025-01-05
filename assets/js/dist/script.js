56e1b6fa0c71959875b6f470b716ff70f1cef9ce
Curtis Wirtz <supcurtis@gmail.com>
2025-01-05 03:56:08 -0800

    add intersection observer for entry animations

--- assets/js/dist/script.js
+++ assets/js/dist/script.js
@@ -1,4 +1,31 @@
 "use strict";
+// Scroll triggered animations: simple intersection observer
+var animationDriver = function () {
+    // console.log('animation driver ran');
+    var items = document.querySelectorAll(".step");
+    var callback = function (entries, observer) {
+        // console.log('callback ran');
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting && entry.target.classList.contains("pre-animated")) {
+                // console.log(entry);
+                observer.unobserve(entry.target);
+                // trigger animation
+                entry.target.classList.remove("pre-animated");
+            }
+        });
+    };
+    var animationObserver = new IntersectionObserver(callback, {
+        rootMargin: "0px 0px -20% 0px",
+        threshold: 0
+    });
+    var observeItems = function (items) {
+        items.forEach(function (item) {
+            item.classList.add("pre-animated");
+            animationObserver.observe(item);
+        });
+    };
+    observeItems(items);
+};
 document.addEventListener("DOMContentLoaded", function () {
     // Reset the playing of animated gif on every load by appending '?RANDOM_NUMBER' to the src, forcing image load
     function generateNumber() {
@@ -19,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
     resetGif();
-    console.log("testxxxxing");
+    // trigger scroll entry animations
+    animationDriver();
 });
 //# sourceMappingURL=script.js.map
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,3 +1,36 @@
+// Scroll triggered animations: simple intersection observer
+const animationDriver = () => {
+  // console.log('animation driver ran');
+  const items = document.querySelectorAll(".step");
+
+  const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    // console.log('callback ran');
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.target.classList.contains("pre-animated")) {
+        // console.log(entry);
+
+        observer.unobserve(entry.target);
+
+        // trigger animation
+        entry.target.classList.remove("pre-animated");
+      }
+    });
+  };
+
+  const animationObserver = new IntersectionObserver(callback, {
+    rootMargin: "0px 0px -20% 0px",
+    threshold: 0
+  });
+
+  const observeItems = (items: NodeListOf<Element>) => {
+    items.forEach((item) => {
+      item.classList.add("pre-animated");
+      animationObserver.observe(item);
+    });
+  };
+  observeItems(items);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   // Reset the playing of animated gif on every load by appending '?RANDOM_NUMBER' to the src, forcing image load
   function generateNumber(): string {
@@ -22,5 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   resetGif();
 
-  console.log("testxxxxing");
+  // trigger scroll entry animations
+  animationDriver();
 });
